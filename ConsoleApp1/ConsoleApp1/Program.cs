7af28e8f2de1e128ceeae1e70ca6753ff3720002
Cristian P.J <cristianpj@msn.com>
2019-11-06 22:03:50 -0300

    aleração

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,8 +8,9 @@ namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            
         }
     }
 }
